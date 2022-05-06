a113f8b7442ad1b074b987ccebc2f9bee01559b9
Ubuntu <ap_admin@apleona-vm-optimise-v2-001.ob3zsw4tmo1edaokg0u3regdjd.ax.internal.cloudapp.net>
2022-05-06 14:35:37 +0000

    test

--- app/config/config.php
+++ app/config/config.php
@@ -2,14 +2,14 @@
   // DB Params
   //define('DB_HOST', '20.229.43.101');
   define('DB_HOST', 'localhost');
-  define('DB_USER', 'root');
-  define('DB_PASS', 'U55C3rr1t0s!');
+  define('DB_USER', 'apleona');
+  define('DB_PASS', 'Qwerty123!');
   define('DB_NAME', 'apleona_waste');
 
   // App Root
   define('APPROOT', dirname(dirname(__FILE__)));
   // URL Root
-  define('URLROOT', 'http://localhost/apleonawaste');
+  define('URLROOT', 'http://localhost');
   //define('URLROOT', 'https://www.apleona.ie/apleonawaste');
   // Site Name
   define('SITENAME', 'Apleona Waste Management ');
